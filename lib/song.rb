bea56990ff73a04b46869548c48611429217a5f0
Roberto Medina <robby.medina562@gmail.com>
2019-09-11 14:18:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
     genre_count
   end
   def self.artist_count
-    artist_count = {}
+    artist_count = Hash.new(0) # {}
     @@artists.each do |artist|
       if artist_count[artist]
         artist_count[artist] +=1
